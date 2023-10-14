13a8879b66dc886ba3efa40cfda392b2bc10fecb
Hasibul <skhasibulhossain16@gmail.com>
2023-10-15 01:25:28 +0530

    ready for test

--- components/useFetch.js
+++ components/useFetch.js
@@ -1,3 +1,25 @@
 //write your code here
-
-export default useFetch
+import { useState, useEffect } from "react";
+async function getAPIData(URL) {
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+}
+function useFetch(URL) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    getAPIData(URL)
+      .then((responseData) => {
+        setData(responseData);
+      })
+      .catch((e) => {
+        setData([]);
+        setError(e);
+      });
+    setLoading(false);
+  }, [URL]);
+  return { data, loading, error };
+}
+export default useFetch;
